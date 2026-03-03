44b1993161cb367728e4848e8df9619f3f928a09
jiexdrop <jorge.noogueira@gmail.com>
2026-03-03 19:52:17 +0000

    magnetaktic added to solution

--- magnetaktic/Program.cs
+++ magnetaktic/Program.cs
@@ -1,14 +1,2 @@
-﻿using System;
-try
-{
-    Console.WriteLine("Starting...");
-    using var game = new magnetaktic.Game1();
-    Console.WriteLine("Game created...");
-    game.Run();
-    Console.WriteLine("Game exited normally.");
-}
-catch (Exception ex)
-{
-    Console.WriteLine("CRASH: " + ex.ToString());
-    Console.ReadLine(); // keeps the window open
-}
+﻿using var game = new magnetaktic.Game1();
+game.Run();
