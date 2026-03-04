fdffcef32a9e09a01765eb40770cf5b28f5b8fac
jiexdrop <jorge.noogueira@gmail.com>
2026-03-04 10:01:12 +0000

    Texture atlas

--- magnetaktic/Game1.cs
+++ magnetaktic/Game1.cs
@@ -2,14 +2,17 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoGameLibrary;
+using MonoGameLibrary.Graphics;
 
 namespace magnetaktic;
 
 public class Game1 : Core
 {
-    private SpriteBatch _spriteBatch;
+    // texture region that defines the slime sprite in the atlas.
+    private TextureRegion _slime;
 
-    private Texture2D tileSet;
+    // texture region that defines the bat sprite in the atlas.
+    private TextureRegion _bat;
 
     public Game1() : base("Magnetaktic", 1280, 720, false)
     {
@@ -26,8 +29,23 @@ public class Game1 : Core
 
     protected override void LoadContent()
     {
-        _spriteBatch = Core.SpriteBatch;
-        tileSet = Content.Load<Texture2D>("Tiles/tileset");
+        // Load the atlas texture using the content manager
+        Texture2D atlasTexture = Content.Load<Texture2D>("images/atlas");
+
+        //  Create a TextureAtlas instance from the atlas
+        TextureAtlas atlas = new TextureAtlas(atlasTexture);
+
+        // add the slime region to the atlas.
+        atlas.AddRegion("slime", 0, 0, 20, 20);
+
+        // add the bat region to the atlas.
+        atlas.AddRegion("bat", 20, 0, 20, 20);
+
+        // retrieve the slime region from the atlas.
+        _slime = atlas.GetRegion("slime");
+
+        // retrieve the bat region from the atlas.
+        _bat = atlas.GetRegion("bat");
     }
 
     protected override void Update(GameTime gameTime)
@@ -42,10 +60,22 @@ public class Game1 : Core
 
     protected override void Draw(GameTime gameTime)
     {
+        // Clear the back buffer.
         GraphicsDevice.Clear(Color.CornflowerBlue);
 
-        // TODO: Add your drawing code here
+        // Begin the sprite batch to prepare for rendering.
+        SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
+
+        // Draw the slime texture region at a scale of 4.0
+        _slime.Draw(SpriteBatch, Vector2.Zero, Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 0.0f);
+
+        // Draw the bat texture region 10px to the right of the slime at a scale of 4.0
+        _bat.Draw(SpriteBatch, new Vector2(_slime.Width * 4.0f + 10, 0), Color.White, 0.0f, Vector2.One, 4.0f, SpriteEffects.None, 1.0f);
+
+        // Always end the sprite batch when finished.
+        SpriteBatch.End();
 
         base.Draw(gameTime);
     }
+
 }
